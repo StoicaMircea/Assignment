73fc26fceb0576a8298fb81c5b43096e8d6ea32a
StoicaMircea <theunknownhack@protonmail.com>
2022-09-22 22:43:32 +0300

    Working on assignment

--- src/Client.java
+++ src/Client.java
@@ -3,7 +3,27 @@ import java.io.*;
 import java.util.*;
 
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Scanner numere = new Scanner(System.in);
+        System.out.print("Introduceti primul numar: ");
+        int nr1 = numere.nextInt();
 
+        System.out.print("Introduceti al doilea numar: ");
+        int nr2 = numere.nextInt();
+
+        System.out.println("Numerele adaugate sunt: " + nr1 + " si " + nr2);
+        String rezultat = nr1 + "+" + nr2;
+        String line = null;
+        try(Socket socket = new Socket("localhost", 1080);
+            BufferedReader bis = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedOutputStream bos = new BufferedOutputStream(socket.getOutputStream())) {
+                bos.write((rezultat).getBytes());
+                bos.flush();
+
+            line = bis.readLine();
+            System.out.println(line);
+            } catch (IOException exception) {
+                System.out.println(exception.getMessage());
+        }
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,31 @@
+import java.io.*;
+import java.net.*;
+import java.nio.*;
+
+import static java.lang.Integer.sum;
+
 public class Server {
     public static void main(String[] args) {
+        System.out.println("Programul ruleaza!");
+        try(ServerSocket serverSocket = new ServerSocket(1080);
+            Socket client = serverSocket.accept();
+            BufferedReader bis = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            BufferedOutputStream bos = new BufferedOutputStream(client.getOutputStream())) {
 
+            System.out.println("Clientul s-a conectat!");
+            System.out.println("Mesajul de la client: ");
+
+            String[] array = null;
+            String line = bis.readLine();
+            while(line != null && !line.equals("")){
+                array = line.split("\\+", 2);
+                System.out.println(Integer.sum(Integer.parseInt(array[0]), Integer.parseInt(array[1])));
+                bos.write(Integer.sum(Integer.parseInt(array[0]),Integer.parseInt(array[1])));
+                bos.flush();
+                line = bis.readLine();
+            }
+        } catch(IOException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
